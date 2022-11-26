4e717e82ec5c1740eb15e98525a2458c54d3f60e
r0shish <roshish152002@gmail.com>
2022-11-26 15:00:03 +0545

    OODP Week 1 - Workshop

---
+++ Third Semester/5CS021 - Object-Oriented Design and Programming/Week 1 - Workshop/Car/Car.java
@@ -0,0 +1,17 @@
+class Car {
+    String brand;
+
+    public Car(String brand) {
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public static void main(String[] args) {
+        Car ford = new Car("Ford");
+        String brand = ford.getBrand();
+        System.out.println(brand);
+    }
+}
