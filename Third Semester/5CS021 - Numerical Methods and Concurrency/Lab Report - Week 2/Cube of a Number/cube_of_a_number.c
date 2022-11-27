dbf989a0eebdeab18953388e28224118a8591671
r0shish <roshish152002@gmail.com>
2022-11-27 07:59:26 +0545

    Simultaneous Equation

--- Third Semester/5CS021 - Numerical Methods and Concurrency/Lab Report - Week 2/Cube of a Number/cube_of_a_number.c
+++ Third Semester/5CS021 - Numerical Methods and Concurrency/Lab Report - Week 2/Cube of a Number/cube_of_a_number.c
@@ -4,6 +4,7 @@ int cube(int num)
 {
     return num * num * num;
 }
+
 int main()
 {
     int n = 5;
---
+++ Third Semester/5CS021 - Numerical Methods and Concurrency/Lab Report - Week 2/Simultaneous Equation/simultaneous_equation.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void equation(double a, double b, double c, double d,
+              double e, double f)
+{
+    double x, y;
+    x = (f * b - e * c) / (b * d - a * e);
+    y = (c - a * x) / b;
+
+    printf("The point where the two lines cross each other is (%g, %g)", x, y);
+}
+int main()
+{
+    equation(10, 5, 60, 2, 15, 40);
+    return 0;
+}
