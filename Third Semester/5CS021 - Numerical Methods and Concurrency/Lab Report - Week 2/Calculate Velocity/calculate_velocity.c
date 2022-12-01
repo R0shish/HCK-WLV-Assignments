211a8cd21e080d7b7b9d00bbdcebf3faa0ba2121
r0shish <roshish152002@gmail.com>
2022-12-01 08:16:19 +0545

    fix (calculate_velocity): remove user input and add NAN

--- Third Semester/5CS021 - Numerical Methods and Concurrency/Lab Report - Week 2/Calculate Velocity/calculate_velocity.c
+++ Third Semester/5CS021 - Numerical Methods and Concurrency/Lab Report - Week 2/Calculate Velocity/calculate_velocity.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
 #include <math.h>
 
-int velocityCalc(int v, int u, int a, int t)
+float velocityCalc(float v, float u, float a, float t)
 {
-    if (isnan(v) || v == 0)
+    if (isnan(v))
     {
         v = u + a * t;
         return v;
     }
-    else if (isnan(u) || u == 0)
+    else if (isnan(u))
     {
         u = v - a * t;
         return u;
     }
-    else if (isnan(a) || a == 0)
+    else if (isnan(a))
     {
         a = (v - u) / t;
         return a;
     }
-    else if (isnan(t) || t == 0)
+    else if (isnan(t))
     {
         t = (v - u) / a;
         return t;
@@ -32,16 +32,10 @@ int velocityCalc(int v, int u, int a, int t)
 int main()
 {
     float v, u, a, t;
-    printf("Enter 0 for the unknown value\n");
-    printf("Enter the value of v: ");
-    scanf("%f", &v);
-    printf("Enter the value of u: ");
-    scanf("%f", &u);
-    printf("Enter the value of a: ");
-    scanf("%f", &a);
-    printf("Enter the value of t: ");
-    scanf("%f", &t);
+    v = NAN, u = 20, a = 50, t = 20;
+    // v = 40, u = 20, a = 50, t = NAN;
+    // v = 40, u = 30, a = NAN, t = 60;
 
-    printf("The value of unknown variable is %d.\n", velocityCalc(v, u, a, t));
+    printf("The value of unknown variable is %g.\n", velocityCalc(v, u, a, t));
     return 0;
 }
