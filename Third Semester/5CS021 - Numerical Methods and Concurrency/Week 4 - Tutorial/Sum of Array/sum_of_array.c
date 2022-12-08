affdec3c6787b47640c7a60253a16df6af8bb037
r0shish <roshish152002@gmail.com>
2022-12-08 08:12:34 +0545

    fix: static array size

--- Third Semester/5CS021 - Numerical Methods and Concurrency/Week 4 - Tutorial/Sum of Array/sum_of_array.c
+++ Third Semester/5CS021 - Numerical Methods and Concurrency/Week 4 - Tutorial/Sum of Array/sum_of_array.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int arr[5] = {1, 2, 3, 4, 5};
+    int arr[] = {1, 2, 3, 4, 5, 6};
     int sum = 0;
     int *p = &arr[0];
     for (int i = 0; i < sizeof(arr) / sizeof(int); i++, p++)
