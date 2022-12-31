d72b5e9654dc70fc8289c0febeac605f06235238
r0shish <roshish152002@gmail.com>
2022-12-31 22:37:21 +0545

    5CS021: handle file handling error and minor fix

--- Third Semester/5CS021 - Numerical Methods and Concurrency/Week 6 - Workshop/file_handling.c
+++ Third Semester/5CS021 - Numerical Methods and Concurrency/Week 6 - Workshop/file_handling.c
@@ -4,18 +4,31 @@ int main()
 {
     FILE *fptr;
     int x, y;
-    fptr = fopen("test6.txt", "w");
+    char filename[] = "test.txt";
+
+    fptr = fopen(filename, "w");
+    if (fptr == NULL)
+    {
+        printf("Error opening file!\n");
+    }
+
     printf("Enter the values of x: ");
     scanf("%d", &x);
     printf("Enter the values of y: ");
     scanf("%d", &y);
     fprintf(fptr, "The value of x is %d. The value of y is %d.", x, y);
     fclose(fptr);
-    fptr = fopen("test6.txt", "r");
+
+    fptr = fopen(filename, "r");
+    if (fptr == NULL)
+    {
+        printf("Error reading file!\n");
+    }
     while (!feof(fptr))
     {
         fscanf(fptr, "The value of x is %d. The value of y is %d.", &x, &y);
     }
+
     printf("The final values are: %d and %d.\n", x, y);
     fclose(fptr);
     return 0;
