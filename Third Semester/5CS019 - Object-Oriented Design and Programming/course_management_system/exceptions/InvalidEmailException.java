80dbeeaf6c049ca89cc066196a140edd3f95128e
r0shish <roshish152002@gmail.com>
2023-01-21 16:42:20 +0545

    Migrate course_management_system to standalone repo

--- Third Semester/5CS019 - Object-Oriented Design and Programming/course_management_system/exceptions/InvalidEmailException.java
+++
@@ -1,9 +0,0 @@
-package course_management_system.exceptions;
-
-public class InvalidEmailException extends Exception {
-
-	private static final long serialVersionUID = -8145316108548232390L;
-	public InvalidEmailException(String message) {
-		super(message);
-	}
-}
--- Third Semester/5CS019 - Object-Oriented Design and Programming/course_management_system/exceptions/InvalidPasswordException.java
+++
@@ -1,9 +0,0 @@
-package course_management_system.exceptions;
-
-public class InvalidPasswordException extends Exception {
-
-	private static final long serialVersionUID = 3761449227809409477L;
-	public InvalidPasswordException(String message) {
-		super(message);
-	}
-}
